b6042f5f99355b6f2426c6d01e0579fde0bae7e3
f1ne2 <alextomilovski@gmail.com>
2021-02-10 16:15:50 +0300

    postgre sql in enviroment virable

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = \
-        'postgresql://postgres:SNekH2233@localhost:5432/quiz'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("POSTGRE_SQL") or \
+                              'postgresql://postgres:SNekH2233@localhost:5432/quiz'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
