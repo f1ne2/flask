a781a45888782d26762f7b7e2b8029fc7cf4527b
Stas <stas220693@gmail.com>
2021-03-04 21:13:26 +0300

    create auth

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get("POSTGRE_SQL") or \
-                              'postgresql://postgres:SNekH2233@localhost:5432/quiz'
+                              'postgresql://postgres:admin@localhost:5432/quiz'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
