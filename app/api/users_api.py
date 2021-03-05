bde7babb8bc1988d9c08ec84e621d89b12ac5283
Stas <stas220693@gmail.com>
2021-03-05 21:08:10 +0300

    create auth 05-03-21

--- app/api/users_api.py
+++ app/api/users_api.py
@@ -6,7 +6,6 @@ from flask import jsonify, wrappers, request, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 
-import uuid
 import jwt
 import datetime
 
@@ -23,7 +22,7 @@ def token_required(f):
             return jsonify({'message': 'Token is missing!'}), 401
 
         try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms="HS256")
             current_user = Users.query.filter_by(id=data['id']).first()
         except:
             return jsonify({'message': 'Token is invalid!'}), 401
@@ -47,17 +46,10 @@ def login() -> wrappers.Response:
 
     if check_password_hash(user.password, auth.password):
         token = jwt.encode({'id': user.id,
-                            'exp':
-                                datetime.datetime.utcnow() + datetime.timedelta(minutes=10)},
+                            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=10)},
                            app.config['SECRET_KEY'])
 
-        # token = jwt.encode({'id': user.id}, app.config['SECRET_KEY'])
-
-        return jsonify({'token': token.decode("utf-8")})
-
-        # return jsonify({'token': token})
-        # return jsonify({'token': token.encode().decode()})
-        # return jsonify({'token': token.encode("windows-1252").decode("utf-8")})
+        return jsonify({'token': token})
 
     return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
@@ -77,7 +69,7 @@ def create_user() -> wrappers.Response:
 
 @app.route('/user/<int:id>', methods=['PUT'])
 @token_required
-def promote_user(id: int, current_user) -> wrappers.Response:
+def promote_user(current_user, id: int) -> wrappers.Response:
     if not current_user.admin:
         return jsonify({'message': 'Cannot perform that function!'})
 
@@ -114,7 +106,7 @@ def get_all_users(current_user) -> wrappers.Response:
 
 @app.route('/user/<int:id>', methods=['GET'])
 @token_required
-def get_one_user(id: int, current_user) -> wrappers.Response:
+def get_one_user(current_user, id: int) -> wrappers.Response:
     if not current_user.admin:
         return jsonify({'message': 'Cannot perform that function!'})
 
@@ -133,7 +125,7 @@ def get_one_user(id: int, current_user) -> wrappers.Response:
 
 @app.route('/user/<int:id>', methods=['DELETE'])
 @token_required
-def delete_user(id: int, current_user) -> wrappers.Response:
+def delete_user(current_user, id: int) -> wrappers.Response:
     if not current_user.admin:
         return jsonify({'message': 'Cannot perform that function!'})
 
--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 
 class Config(object):
     SECRET_KEY = 'you-will-never-guess'
-    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get("POSTGRE_SQL") or \
                               'postgresql://postgres:admin@localhost:5432/quiz'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
