e46767413b6cae464957563fc94b98c5143cccac
f1ne2 <alextomilovski@gmail.com>
2021-04-07 10:53:27 +0300

    initial deploy

--- quiz.py
+++ quiz.py
@@ -6,3 +6,7 @@ from app.models import Categories, Questions, Answers
 def make_shell_context():
     return {'db': db, 'Categories': Categories, 'Questions': Questions,
             'Answers': Answers}
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port="5000")
